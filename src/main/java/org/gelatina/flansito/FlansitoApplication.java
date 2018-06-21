ae58e700629235a14d36332a041f04d9ded239ff
Juan Carlos Campos <rapidclimate@gmail.com>
2018-06-20 23:52:35 -0500

    Initial commit

---
+++ src/main/java/org/gelatina/flansito/FlansitoApplication.java
@@ -0,0 +1,12 @@
+package org.gelatina.flansito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlansitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlansitoApplication.class, args);
+	}
+}
---
+++ src/test/java/org/gelatina/flansito/FlansitoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.gelatina.flansito;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlansitoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
